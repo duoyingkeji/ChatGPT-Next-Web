13bd21615e97d4a1935b20ee0cc6a89e13c70f35
DuoYingShuKe <d470969047h@gmail.com>
2023-04-04 10:14:27 +0800

    修改title

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -103,9 +103,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>嗖嗖AI助手</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
